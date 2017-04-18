afcd744bd0cdc01428a3033c487684f502573a61
Roger Forner Fabre <sir.rogerforner@gmail.com>
2017-04-18 16:18:39 +0200

    Hem fet llum boot i hem afegit propietats al calendari.

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,9 @@ Route::get('/', function () {
 });
 
 Route::group(['middleware' => 'auth'], function () {
-    //    Route::get('/link1', function ()    {
-//        // Uses Auth Middleware
-//    });
+    Route::get('/calendar', function ()    {
+        return view('calendar');
+    });
 
     //Please do not remove this if you want adminlte:route and adminlte:link commands to works correctly.
     #adminlte_routes
